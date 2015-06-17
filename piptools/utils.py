d5495bd66e57a928fe44f2b95e4d782a308f3456
Daniel Hahler <git@thequod.de>
2015-06-17 05:59:06 +0200

    fixup! Re-add support for non-editable VCS urls; fix pinning

--- piptools/utils.py
+++ piptools/utils.py
@@ -21,11 +21,16 @@ def format_requirement(ireq):
     Generic formatter for pretty printing InstallRequirements to the terminal
     in a less verbose way than using its `__str__` method.
     """
-    if ireq.link and not ireq.link.is_artifact and ireq.source_dir:
-        assert os.path.exists(ireq.source_dir)
-        line = get_src_requirement(dist=ireq.get_dist(),
-                                   location=ireq.source_dir,
-                                   find_tags=True)
+    if ireq.link:
+        if (not ireq.link.is_artifact and ireq.source_dir and
+                # NOTE: might be called for debug logging, where the source_dir
+                # does not exist yet!
+                os.path.exists(ireq.source_dir)):
+            line = get_src_requirement(dist=ireq.get_dist(),
+                                       location=ireq.source_dir,
+                                       find_tags=True)
+        else:
+            line = str(ireq.link)
     else:
         line = str(ireq.req)
 
